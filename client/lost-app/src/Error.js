fc2acbbb412dbc1250211ff36059c22924f7f722
cristinanoelle11 <30802336+cristinanoelle11@users.noreply.github.com>
2022-03-01 07:42:19 -0500

    creating the base pages

---
+++ client/lost-app/src/Error.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+const Error = () => {
+    return (
+        <div align='center'>
+          <h1>We're sorry but an ERROR has occured.</h1>
+        </div>
+    )
+}
+
+export default Error
---
+++ client/lost-app/src/Settings.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+const Settings = () => {
+    return (
+        <div align='center'>
+          <h1>This is the Settings page</h1>
+        </div>
+    )
+}
+
+export default Settings
