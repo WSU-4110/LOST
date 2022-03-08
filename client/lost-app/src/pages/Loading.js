00b7a4d5261f332893b774a6284e62218e33e958
cristinanoelle11 <30802336+cristinanoelle11@users.noreply.github.com>
2022-03-08 13:41:18 -0500

    centering loading spinner

--- client/lost-app/src/pages/Loading.js
+++ client/lost-app/src/pages/Loading.js
@@ -4,7 +4,8 @@ import {Audio} from 'react-loader-spinner';
 
 const Loading = () => {
     return (
-        <div class = "mystyle">
+        <div className="mystyle">
+            <h1>Loading...</h1>
            <Audio color="#00BFFF" height={80} width={80} />
         </div>
     )
