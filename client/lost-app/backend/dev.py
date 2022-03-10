e642a389e5bfd56c7c2912d978e4882987713645
Anthony Jones <70544335+ajones00@users.noreply.github.com>
2022-03-10 17:00:52 -0500

    creation of getnewToken function
    checks if token has been stored or if it is expired, and refreshes the token if needed. returns token and token validity.

--- client/lost-app/backend/dev.py
+++ client/lost-app/backend/dev.py
@@ -50,6 +50,30 @@ def logout():
     #after token has been removed from session, redirect user to homepage of Lost
     return redirect('http://localhost:3000')
 
+
+# Checks to see if token is valid and gets a new token if not
+@app.route('/getnewToken')
+def get_token():
+    token_valid = False
+    token_info = session.get("token_info", {})
+    
+    # Checking if the session already has a token stored
+    if not (session.get('token_info', False)):
+        token_valid = False
+        return token_info, token_valid
+
+    # Checking if token has expired
+    now = int(time.time())
+    is_token_expired = session.get('token_info').get('expires_at') - now < 60
+
+    # Refreshing token if it has expired
+    if (is_token_expired):
+        sp_oauth = create_spotify_oauth()
+        token_info = sp_oauth.refresh_access_token(session.get('token_info').get('refresh_token'))
+
+    token_valid = True
+    return token_info, token_valid
+
 # debug=true when developing
 if __name__ == "__main__":
     app.run(debug=True)
