4ed9006e82de73c240d4f72167ed017e9c78d3a1
Kylie Lee <kcgalee@gmail.com>
2022-03-15 23:58:29 -0400

    Music Player Page

--- client/lost-app/src/pages/MusicPlayer.js
+++ client/lost-app/src/pages/MusicPlayer.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Loading from './Loading';
 import { Search } from '../components/Search';
+import '../styles/MusicPlayer.css';
 
 
 const MusicPlayer = () => {
@@ -31,17 +32,29 @@ const MusicPlayer = () => {
 
     return (
         <div className="mainContainer">
-          <div className="subContainer">
-            
+          <div className="nav">
+            <div className="logo">
+              <img src="https://i.imgur.com/NMLpcKj.jpeg" alt="logo"/>
+            </div>
           </div>
-          <div className="subContainer">
-            <h1>ATTRIBUTES</h1>
-          </div>
-          <div className="subContainer">
-            <h1>ADD</h1>
+          <div className="bodyContainer">
+            <div className="subContainer" id="player">
+              <Search></Search>
+              <div className="album">
+                <img width={"70%"} src="https://i.imgur.com/4HPKiov.jpeg" alt="album cover"/>
+              
+              </div>
+            </div>
+            <div className="subContainer">
+              <h1>ATTRIBUTES</h1>
+            </div>
+            <div className="subContainer">
+              <h1>ADD</h1>
+              <Search></Search>
+            </div>
           </div>
 
-          <div className="test">
+          {/* <div className="test">
           {(typeof data.members === 'undefined') ? (
             <p>Loading...</p>
           ) : (
@@ -49,8 +62,8 @@ const MusicPlayer = () => {
               <p key ={i}>{member}</p>
             ))
           )}
-        </div>
-        <Search></Search>
+        </div> */}
+        
 
         </div>
         
