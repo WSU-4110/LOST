5df3fe7792dbbf9ceb6723238f7e6d1a338f43e0
Anthony Jones <70544335+ajones00@users.noreply.github.com>
2022-03-28 23:03:58 -0400

    testing search results in frontend

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -11,8 +11,10 @@ export default class Home extends Component {
         };
         this.authenticateSpotify = this.authenticateSpotify.bind(this);
         this.authenticateSpotify();
+        this.getSearchResults();
     }
 
+    storage;
     /*
     // Get Details for the Users Home Page 
     getHomeDetails() {
@@ -43,8 +45,17 @@ export default class Home extends Component {
             });
     }
 
+    //tester function to show search results request
+    getSearchResults() {
+        fetch("http://127.0.0.1:8000/spotify/searchAPI")
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+            });
+    }
+
     //User logout
-    logout() {
+    logoutofSpotify() {
         fetch('/spotify/logout-user');
         window.open('https://www.spotify.com/us/logout/');
         setTimeout(function () {
@@ -66,7 +77,7 @@ export default class Home extends Component {
                     <Button
                         variant="contained"
                         color="secondary"
-                        onClick={this.logout}
+                        onClick={this.logoutofSpotify}
                     >
                         Log Out
                     </Button>
