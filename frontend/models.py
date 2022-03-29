0342a6eb1ea38b839b89d94cdd49f683d1903a53
Masrur Tajwar <79229270+masrurtajwar@users.noreply.github.com>
2022-03-29 00:07:21 -0400

    Update composite key

--- frontend/models.py
+++ frontend/models.py
@@ -4,15 +4,16 @@ from django.db import models
 
 # Creating Database model
 class Database(models.Model):
-    class Meta:
-        composite_key = ('userEmail', 'trackID')
 
     userEmail = models.IntegerField(unique=True)
-    trackID = models.IntegerField(max_length=50, unique=True)
-    loudness = models.IntegerField(max_length=50)
+    trackID = models.IntegerField(unique=True)
+    loudness = models.IntegerField()
     location = models.CharField(max_length=200)
     mood = models.CharField(max_length=200)
     activity = models.CharField(max_length=200)
     custom_attr1 = models.CharField(max_length=200)
     custom_attr2 = models.CharField(max_length=200)
     custom_attr3 = models.CharField(max_length=200)
+
+    class Meta:
+        unique_together = (('userEmail', 'trackID'),)
