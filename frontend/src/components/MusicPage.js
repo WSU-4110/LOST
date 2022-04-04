31fade00fdc260622cc8cc32422c152978cbceb0
Anthony Jones <70544335+ajones00@users.noreply.github.com>
2022-04-04 10:35:57 -0400

    frontend call to spotify search method

--- frontend/src/components/MusicPage.js
+++ frontend/src/components/MusicPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Loading from './Loading';
 
 
@@ -9,24 +9,50 @@ const MusicPage = () => {
       setIsLoading(false);
     }, 1500);
   })
-  if(isLoading){
-    return(
+  if (isLoading) {
+    return (
       <Loading />
     );
   }
-    return (
-      <div className="mainContainer">
+
+  const get_SearchResults = e => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        searchStr: e,
+      }),
+    };
+    fetch("http://127.0.0.1:8000/spotify/searchAPI", requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        //to get a single item within the 'albums' list
+        console.log(data['albums']['items'][0])
+
+        //to get all items within the 'albums' list
+        console.log(data['albums']['items'][0])
+
+        //the same can be done with the tracks and artists lists
+      });
+  }
+
+
+  return (
+    <div className="mainContainer">
       <div className="nav">
         <div className="logo">
-          <img src="https://i.imgur.com/NMLpcKj.jpeg" width={"5%"} alt="logo"/>
+          <img src="https://i.imgur.com/NMLpcKj.jpeg" width={"5%"} alt="logo" />
         </div>
       </div>
       <div className="bodyContainer">
         <div className="subContainer" id="player">
-          {/* <Search></Search> */}
+          <div class="search">
+            <input type="text" class="search-bar" placeholder="Search" onChange={e => get_SearchResults(e.target.value)} />
+            <button><i class="material-icons">searchelp</i></button>
+          </div>
           <div className="album">
-            <img width={"70%"} src="https://i.imgur.com/4HPKiov.jpeg" alt="album cover"/>
-          
+            <img width={"70%"} src="https://i.imgur.com/4HPKiov.jpeg" alt="album cover" />
+
           </div>
         </div>
         <div className="subContainer">
@@ -37,8 +63,8 @@ const MusicPage = () => {
           {/* <Search></Search> */}
         </div>
       </div>
-        </div>
-    )
+    </div>
+  )
 }
 
 export default MusicPage
