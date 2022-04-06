ec422ef82f1ac5e56ff3b03011a8042311cde644
Kylie Lee <kcgalee@gmail.com>
2022-04-05 22:52:26 -0400

    Homework4 Factory Method

--- frontend/src/components/Attributes.js
+++ frontend/src/components/Attributes.js
@@ -2,15 +2,25 @@ import '../styles/Attributes.css';
 import React from 'react';
 
 
-const Attributes = () => {
-    
-    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+const Create = () => {
+    let width = 100;
+    let height = auto;
+    let Color = Math.floor(Math.random() * 16777215).toString(16);
+    var title;
  
     return (
-        <div style={{ backgroundColor: "#" + `${randomColor}` }} className="bubble">
+        <div style={{ backgroundColor: "#" + `${Color}` }} className="bubble">
             <h4 className="description">attribute 2</h4>
         </div>
     )
 }
 
-export default Attributes
+export default Create
+
+const preDefined = () => {
+    Create(this.definedAttributes);
+}
+
+const custom = () => {
+    Create(this.userInput);
+}
