b6da489cbf330e7817965e73898df34ef31d0329
Kylie Lee <kcgalee@gmail.com>
2022-04-10 17:26:16 -0400

    Update

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -60,7 +60,7 @@ export default class Home extends Component {
                 <nav>
                     <div class='navBar'>
                         <div class='musicPlayerLink'>
-                            <Button color="secondary" class="songBtn" variant="contained" to="/Song" component={Link}>
+                            <Button color="secondary" class="songBtn" variant="contained" to="/MusicPage" component={Link}>
                                 SONGS
                             </Button>
                         </div>
