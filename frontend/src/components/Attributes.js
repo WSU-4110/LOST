7db1916f6ee59fb01de87520d93e57bb0bde9d97
Anthony Jones <70544335+ajones00@users.noreply.github.com>
2022-04-16 09:09:00 -0400

    allow user to add, delete, and clear attributes

--- frontend/src/components/Attributes.js
+++ frontend/src/components/Attributes.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
 
-const Attributes = () => {
-    
+const Attributes = (attrType, attrName) => {
+
     let randomColor = Math.floor(Math.random() * 16777215).toString(16);
- 
+
     return (
-        <div style={{ backgroundColor: "#" + `${randomColor}` }} className="bubble">
-            <h4 className="description">attribute</h4>
-        </div>
+        "<div style='background-color: #" + randomColor + "' class='bubble'><h4 class=" + attrName + " id=" + attrType + ">" + attrName + "</h4></div>"
+
     )
 }
 
--- frontend/src/components/MusicPage.js
+++ frontend/src/components/MusicPage.js
@@ -67,13 +67,249 @@ const MusicPage = () => {
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        /*if (typeof data['mood'] != "object") {
-          console.log('attr name');
-        } else {
-
-        }*/
-
+        middleColumnAttr(data);
+        loadAvailableAttributes(data);
       });
+  }
+
+  const refreshCols = (songData) => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: songData['userEmail'],
+        id: songData['trackID'],
+      }),
+    };
+    fetch("http://127.0.0.1:8000/spotify/finduserSong", requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('refresh cols function');
+        middleColumnAttr(data);
+        loadAvailableAttributes(data);
+      });
+  }
+
+  const middleColumnAttr = (songData) => {
+    var htmlBody = "";
+    var parentT = document.getElementsByClassName("songAttrHolder")[0];
+
+    $("#songAttrHolder div").empty();
+
+    htmlBody += Attributes('Clear', 'Clear Attributes');
+
+    if (typeof songData['location'] != "object") {
+      htmlBody += Attributes('location', songData['location']);
+    }
+
+    if (typeof songData['mood'] != "object") {
+      htmlBody += Attributes('mood', songData['mood']);
+    }
+
+    if (typeof songData['activity'] != "object") {
+      htmlBody += Attributes('activity', songData['activity']);
+    }
+
+    if (typeof songData['custom_attr'] != "object") {
+      htmlBody += Attributes('custom_attr', songData['custom_attr']);
+    }
+
+    if (typeof songData['custom_attrtwo'] != "object") {
+      htmlBody += Attributes('custom_attrtwo', songData['custom_attrtwo']);
+    }
+
+    if (typeof songData['custom_attrthree'] != "object") {
+      htmlBody += Attributes('custom_attrthree', songData['custom_attrthree']);
+    }
+
+    parentT.innerHTML = htmlBody;
+
+    var removeAttr = function (attrBubble) {
+      return function () {
+        var attrType = attrBubble.id;
+        console.log("remove function att type: " + attrType);
+
+        var songID = document.getElementsByClassName("selectedSong")[0].id;
+        console.log('id: ' + songID);
+
+        const requestOptions = {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            type: attrType,
+            id: songID,
+          }),
+        };
+        fetch("http://127.0.0.1:8000/spotify/rmvAttrFromSong", requestOptions)
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            //refreshCols(songID);
+            sendtoDB(songID);
+          });
+      };
+    };
+
+    var clearAttr = function () {
+      return function () {
+        var songID = document.getElementsByClassName("selectedSong")[0].id;
+
+        const requestOptions = {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            id: songID,
+          }),
+        };
+        fetch("http://127.0.0.1:8000/spotify/clear", requestOptions)
+          .then((response) => response.json())
+          .then((data) => {
+            console.log('clear function used');
+            console.log(data);
+            sendtoDB(songID);
+          });
+      };
+    };
+
+    var clearBubble = document.getElementById("Clear");
+    clearBubble.onclick = clearAttr();
+
+    if (typeof songData['location'] != "object") {
+      var bubbleAttr = document.getElementById("location");
+      bubbleAttr.onclick = removeAttr(bubbleAttr);
+    }
+
+    if (typeof songData['mood'] != "object") {
+      bubbleAttr = document.getElementById("mood");
+      bubbleAttr.onclick = removeAttr(bubbleAttr);
+    }
+
+    if (typeof songData['activity'] != "object") {
+      bubbleAttr = document.getElementById("activity");
+      bubbleAttr.onclick = removeAttr(bubbleAttr);
+    }
+
+    if (typeof songData['custom_attr'] != "object") {
+      bubbleAttr = document.getElementById("custom_attr");
+      bubbleAttr.onclick = removeAttr(bubbleAttr);
+    }
+
+    if (typeof songData['custom_attrtwo'] != "object") {
+      bubbleAttr = document.getElementById("custom_attrtwo");
+      bubbleAttr.onclick = removeAttr(bubbleAttr);
+    }
+
+    if (typeof songData['custom_attrthree'] != "object") {
+      bubbleAttr = document.getElementById("custom_attrthree");
+      bubbleAttr.onclick = removeAttr(bubbleAttr);
+    }
+  }
+
+  const loadAvailableAttributes = (songData) => {
+    var parentT = document.getElementsByClassName("Attributes")[0];
+    $("#Attributes div").empty();
+
+    var htmlBody = "";
+
+    var locations = ['gym', 'school', 'work', 'home', 'beach'];
+    var moods = ['happy', 'sad', 'angry', 'soft', 'loud', 'sentimental', 'lonely', 'melancholy'];
+    var activities = ['studying', 'cooking', 'sleeping', 'driving', 'walking', 'running', 'cleaning'];
+
+    if (typeof songData['location'] == "object") {
+      for (var i = 0; i < locations.length; i++) {
+        htmlBody += Attributes('location', locations[i]);
+      }
+    }
+
+    if (typeof songData['mood'] == "object") {
+      for (var i = 0; i < moods.length; i++) {
+        htmlBody += Attributes('mood', moods[i]);
+      }
+    }
+
+    if (typeof songData['activity'] == "object") {
+      for (var i = 0; i < activities.length; i++) {
+        htmlBody += Attributes('activity', activities[i]);
+      }
+    }
+
+    var customAttr = [];
+
+    if (typeof songData['custom_attr'] == "object" || typeof songData['custom_attrtwo'] == "object" || typeof songData['custom_attr'] == "object") {
+      if (customAttr.length != 0) {
+        fetch("http://127.0.0.1:8000/spotify/getUserCustomAttr")
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            if (typeof songData['custom_attr'] != "object") {
+              for (var i = 0; i < customAttr.length; i++) {
+                htmlBody += Attributes('custom_attr', customAttr[i]);
+              }
+            } else if (typeof songData['custom_attrtwo'] != "object") {
+              for (var i = 0; i < customAttr.length; i++) {
+                htmlBody += Attributes('custom_attrtwo', customAttr[i]);
+              }
+            } else {
+              for (var i = 0; i < customAttr.length; i++) {
+                htmlBody += Attributes('custom_attrthree', customAttr[i]);
+              }
+            }
+          });
+      }
+    }
+
+    parentT.innerHTML = htmlBody;
+
+    //make onlcick functions here
+    var addAttr = function (tag) {
+      return function () {
+        var attrType = tag.id;
+        console.log("add type: " + attrType);
+        var attrDesc = tag.className;
+        console.log("add desc: " + attrDesc);
+        var songID = document.getElementsByClassName("selectedSong")[0].id;
+        console.log('id: ' + songID);
+
+        const requestOptions = {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            type: attrType,
+            desc: attrDesc,
+            id: songID,
+          }),
+        };
+        fetch("http://127.0.0.1:8000/spotify/addAttribute2Song", requestOptions)
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            sendtoDB(songID);
+          });
+      };
+    };
+
+    var attrBubble;
+
+    if (typeof songData['location'] == "object") {
+      for (var i = 0; i < locations.length; i++) {
+        attrBubble = document.getElementsByClassName(locations[i])[0];
+        attrBubble.onclick = addAttr(attrBubble);
+      }
+    }
+
+    if (typeof songData['mood'] == "object") {
+      for (var i = 0; i < moods.length; i++) {
+        attrBubble = document.getElementsByClassName(moods[i])[0];
+        attrBubble.onclick = addAttr(attrBubble);
+      }
+    }
+
+    if (typeof songData['activity'] == "object") {
+      for (var i = 0; i < activities.length; i++) {
+        attrBubble = document.getElementsByClassName(activities[i])[0];
+        attrBubble.onclick = addAttr(attrBubble);
+      }
+    }
   }
 
   const fillTable = (data) => {
@@ -84,7 +320,6 @@ const MusicPage = () => {
       var row = table.insertRow();
       row.style.cssText = "cursor: pointer;";
 
-      //save for later, needed for rest of webpage functionality
       var sendRowInfo = function (row) {
         return function () {
           var info;
@@ -92,9 +327,7 @@ const MusicPage = () => {
           info = row.getElementsByTagName("td")[1].innerHTML + " " + row.getElementsByTagName("td")[2].innerHTML;
           display[0] = row.getElementsByTagName("td")[1].innerHTML;
           display[1] = row.getElementsByTagName("td")[2].innerHTML;
-          //console.log(rowInfo[0] + " " + rowInfo[1]);
           fillColumn(info, display);
-          //alert("Song Name: " + rowInfo[0] + "\nArtist(s): " + rowInfo[1]);
         };
       };
 
@@ -146,16 +379,13 @@ const MusicPage = () => {
           </div>
           <div className="songPlayer">
             <div className="selectedSong"></div>
-            <div className="flex">
-            </div>
+            <div className="songAttrHolder" id="songAttrHolder"></div>
+            <div className="flex"></div>
           </div>
           <div className="subContainer">
             <div className="translucent">
               <h1>Attributes</h1>
-              <div className="Attributes">
-              
-              <Attributes></Attributes>
-              </div>
+              <div className="Attributes"></div>
             </div>
             <div className="translucent">
               <h1>Add</h1>
