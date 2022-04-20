5be266e9d2c1b7feaeffc299ef50e0718caeb050
kelbel272 <kellyrosewhitlock@gmail.com>
2022-04-19 20:19:00 -0400

    Created Playlist database

--- api/models.py
+++ api/models.py
@@ -28,3 +28,17 @@ class CustomAttributes(models.Model):
 
     class Meta:
         unique_together = (('userEmail', 'attr'),)
+
+class Playlists(models.Model):
+    userEmail = models.CharField(max_length=200)
+    attr = models.CharField(max_length=50)
+    trackID_1 = models.CharField(max_length=200)
+    trackID_2 = models.CharField(max_length=200)
+    trackID_3 = models.CharField(max_length=200)
+    trackID_4 = models.CharField(max_length=200)
+    trackID_5 = models.CharField(max_length=200)
+    trackID_6 = models.CharField(max_length=200)
+    trackID_7 = models.CharField(max_length=200)
+    trackID_8 = models.CharField(max_length=200)
+    trackID_9 = models.CharField(max_length=200)
+    trackID_10 = models.CharField(max_length=200)
