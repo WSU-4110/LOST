1d136fbb48e53565817f571a346ebd1222970954
Masrur Tajwar <79229270+masrurtajwar@users.noreply.github.com>
2022-04-28 00:57:35 -0400

    Update test_unittests.py

--- test_unittests.py
+++ test_unittests.py
@@ -1,25 +1,36 @@
 import unittest   # The test framework
-import spotify
+from spotify.util import get_user_tokens, recentlyPlayed, getUserEmail, addCustomAttr, logout_button, findUserSong
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Lost.test_unittests")
 
 class Test_SpotifyBackend(unittest.TestCase):
     
     def test_getUserTokens(self):
-        self.assertEqual('6yt36lylmzzqeqwimapadiy6m4028snz', '6yt36lylmzzqeqwimapadiy6m4028snz')
+        sessionId = '6yt36lylmzzqeqwimapadiy6m4028snz'
+        user_token = get_user_tokens(sessionId)
+        self.assertEqual('6yt36lylmzzqeqwimapadiy6m4028snz', user_token)
     
     def test_recentlyPlayed(self):
-        self.assertEqual('6d8HN8MqqbqrEUI2bvx0aG','6d8HN8MqqbqrEUI2bvx0aG')   
+        sessionID = '6yt36lylmzzqeqwimapadiy6m4028snz'
+        self.assertEqual('6d8HN8MqqbqrEUI2bvx0aG', recentlyPlayed(sessionID))   
     
     def test_getUserEmail(self):
-        self.assertEqual('masrur.4.real@gmail.com', 'masrur.4.real@gmail.com')
+        sessionID = '6yt36lylmzzqeqwimapadiy6m4028snz'
+        self.assertEqual('masrur.4.real@gmail.com', getUserEmail(sessionID))
 
     def test_addCustomAttr(self): 
-        self.assertEqual('slay', 'slay') 
-    
-    def test_getCustomAttr(self):
-        self.assertEqual('slay','slay')
+        email = 'cristina.powers@powerstribe.net'
+        desc = 'slay'
+        self.assertEqual('slay', addCustomAttr(email, desc)) 
+
+    def test_logoutButton(self):
+        sessionID = '6yt36lylmzzqeqwimapadiy6m4028snz'
+        self.assertEqual(None, logout_button(sessionID))
 
     def test_findUserSong(self):
-        self.assertEqual('6d8HN8MqqbqrEUI2bvx0aG','6d8HN8MqqbqrEUI2bvx0aG')
+        email = 'masrur.4.real@gmail.com'
+        id = '6d8HN8MqqbqrEUI2bvx0aG'
+        self.assertEqual('6d8HN8MqqbqrEUI2bvx0aG', findUserSong(email, id))
         
 if __name__ == '__main__':
     unittest.main()
