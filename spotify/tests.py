4d2e172789948b1362ff045044232ff7924f41bc
kelbel272 <69172048+kelbel272@users.noreply.github.com>
2022-04-28 23:28:07 -0400

    committing 6 unit tests to main

--- spotify/tests.py
+++ spotify/tests.py
@@ -76,5 +76,57 @@ class utilTestCase(TestCase):
         #needs an actual session id from valid user that isnt expired, so the method should return an error and generate an exception
         with self.assertRaises(Exception):
             getSongInfo(2, '12312')
+            
+    def test_create_playlist(self):
+        print('\nCreate Playlist Test\n')
+        
+        #create_playlist takes in a valid, non expired, session id from user
+            # assertRaises generates an exception so an error isnt triggered 
+        with self.assertRaises(Exception):
+            create_playlist(2, 'kellywhitlock', 'TestPlaylistName')
+
+    def test_add_track_to_playlist(self):
+        print('\nAdd Track to Playlist Test\n')
+
+        #add_track_to_playlist takes in a valid, non expired, session id from user
+            # assertRaises generates an exception so an error isnt triggered 
+        with self.assertRaises(Exception):
+            add_track_to_playlist(2, 'playlistID', '12312412')
+
+    def test_get_playlist_tracks(self):
+        print('\nGet Playlist Tracks Test\n')
+
+        #get_playlist_tracks takes in a valid, non expired, session id from user
+            # assertRaises generates an exception so an error isnt triggered 
+        with self.assertRaises(Exception):
+            get_playlist_tracks(2, '4B4jk7gQBOOSevCCMcUj3u')
+       
+
+    def test_findLocationSongs(self):
+        print('\nFind Location Songs\n')
+
+        # Get number of songs stored under the GYM location by User=testEmail  
+        # Answer should be 0 
+        song = findLocationSongs('testEmail', 'gym')
+
+        self.assertEquals(0, song.count())
+
+    def test_findActivitySongs(self):
+         print('\nFind Activity Songs\n')
+        
+        # Get number of songs stored under the RUNNING activity by User=testEmail  
+        # Answer should be 0 
+         song = findActivitySongs('testEmail', 'running')
+
+         self.assertEquals(0, song.count())
+
+    def test_findMoodSongs(self):
+         print('\nFind Mood Songs\n')
+
+        # Get number of songs stored under the Happy mood by User=testEmail  
+        # Answer should be 0 
+         song = findActivitySongs('testEmail', 'happy')
+
+         self.assertEquals(0, song.count())
 
         
