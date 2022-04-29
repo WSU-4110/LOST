6028f8bfa0f86d3d5414f45990c0885655019ca1
Masrur Tajwar <79229270+masrurtajwar@users.noreply.github.com>
2022-04-29 03:14:34 -0400

    Update Masrur/tests.py

--- spotify/tests.py
+++ spotify/tests.py
@@ -1,9 +1,6 @@
-from django.test import TestCase
-
 # Create your tests here.
-from unittest import result
 from django.test import TestCase
-from spotify.util import get_user_tokens, recentlyPlayed, getUserEmail, addCustomAttr, logout_button, findUserSong
+from spotify.util import get_playlist_info, get_user_tokens, is_spotify_authenticated, addCustomAttr, logout_button, refresh_spotify_token
 
 # Create your tests here.
 
@@ -13,15 +10,6 @@ class Test_SpotifyBackend(TestCase):
         sessionId = '6yt36lylmzzqeqwimapadiy6m4028snz'
         user_token = get_user_tokens(sessionId)
         self.assertEqual(None, user_token)
-    
-    def test_recentlyPlayed(self):
-        sessionID = '6yt36lylmzzqeqwimapadiy6m4028snz'
-        self.assertEqual('6d8HN8MqqbqrEUI2bvx0aG', recentlyPlayed(sessionID))   
-    
-    def test_getUserEmail(self):
-        sessionID = '1'
-        result = getUserEmail(sessionID)
-        self.assertEqual('masrur.4.real@gmail.com', result['userEmail'])
 
     def test_addCustomAttr(self): 
         email = 'cristina.powers@powerstribe.net'
@@ -30,19 +18,21 @@ class Test_SpotifyBackend(TestCase):
         self.assertEqual('slay', result['attr']) 
 
     def test_logoutButton(self):
-        sessionID = '6yt36lylmzzqeqwimapadiy6m4028snz'
-        self.assertEqual(None, logout_button(sessionID))
-
-    def test_findUserSong(self):
-        email = 'masrur.4.real@gmail.com' 
-        id = '6d8HN8MqqbqrEUI2bvx0aG'
-        result = findUserSong(email, id)
-        self.assertEqual(None, result)
-
-    def test_logoutButton(self):
         id = '6d8HN8MqqbqrEUI2bvx0aG'
         result = logout_button(id)
         self.assertEqual(None, result)
         
+    def test_isSpotifyAuthenticated(self):
+        id = '6d8HN8MqqbqrEUI2bvx0aG'
+        result = is_spotify_authenticated(id)
+        self.assertEqual(False, result)
 
+    def test_refreshSpotifyToken(self):
+        id = '6d8HN8MqqbqrEUI2bvx0aG'
+        result = refresh_spotify_token(id)
+        self.assertAlmostEqual(None, result)
     
+    # def test_getPlaylistInfo(self):
+    #    id = '6d8HN8MqqbqrEUI2bvx0aG'
+    #    result = get_playlist_info(id)
+    #    self.assertEqual(None, result)
